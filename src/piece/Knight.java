713060fdda2418d96f86c6a4cb820d3a878db960
tatex74 <63496987+tatex74@users.noreply.github.com>
2024-10-22 19:15:29 +0200

    update

---
+++ src/piece/Knight.java
@@ -0,0 +1,35 @@
+package piece;
+import Main.GamePanel;
+import Main.Type;
+import Main.Move;
+import java.util.ArrayList;
+
+public class Knight extends Piece {
+
+    public Knight(int col, int row, int color) {
+        super(col, row, color);
+
+        type = Type.KNIGHT;
+
+        if (color == GamePanel.WHITE) {
+            image = getImage("/piece/w-knight");
+        } else {
+            image = getImage("/piece/b-knight");
+        }
+    }
+    public boolean canMove(int targetCol, int targetRow) {
+        if (isWithinBoard(targetCol, targetRow) ) {
+            //calcul de la distance entre la case de départ et la case d'arrivée du chevalier
+            if (Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 2) {
+                if (isValidSquare(targetCol, targetRow)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    @Override
+    public String getName() {
+        return "Cavalier";
+    }
+}
